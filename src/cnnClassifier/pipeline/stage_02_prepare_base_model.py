831613ed50fe1295acbfa2cfe236fe7f283bae45
Kirti Kumar <kirtikrajsisodiya@gmail.com>
2024-06-06 21:43:36 +0530

    dvc added

--- src/cnnClassifier/pipeline/stage_02_prepare_base_model.py
+++ src/cnnClassifier/pipeline/stage_02_prepare_base_model.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
         logger.info(f"*******************")
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = PrepareBaseModelTrainingPipeline()
-        obj.main(s)
+        obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
         logger.exception(e)
--- src/cnnClassifier/pipeline/stage_04_model_evaluation.py
+++ src/cnnClassifier/pipeline/stage_04_model_evaluation.py
@@ -6,16 +6,16 @@ STAGE_NAME = "Evaluation stage"
 
 
 class EvaluationPipeline:
-    def __init__(self):
+    def __Init__(self):
         pass
 
     def main(self):
         config = ConfigurationManager()
         eval_config = config.get_evaluation_config()
-        evaluation = Evaluation(eval_config)
+        evaluation = Evaluation(config=eval_config)
         evaluation.evaluation()
         evaluation.save_score()
-        evaluation.log_into_mlflow()
+        #evaluation.log_into_mlflow()
 
 
 
